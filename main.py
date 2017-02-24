808a3de8b1f215de513992e1d397495c4ed57288
thomasbn94 <thomasbn94@gmail.com>
2017-02-24 21:33:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -124,7 +124,7 @@ class Crawl(object):
 		try:
 			os.makedirs(path)
 		except OSError as e:
-			if e.errno != errno.EEXIST and os.path.isdir(path):
+			if e.errno == errno.EEXIST and os.path.isdir(path):
 				pass
 			else:
 				raise
